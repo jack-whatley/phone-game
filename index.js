863ed859277a72041ca1ff4dd1ee41b99de4b42c
wheatley1239 <wickselm2005@gmail.com>
2022-03-19 17:53:42 +0000

    fixed server issues (LETS FUCKING GOOO)

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 // https://www.tutorialspoint.com/expressjs/expressjs_routing.htm
 
 const express = require("express");
+const { options } = require("nodemon/lib/config");
 const app = express();
 
 const path = require("path");
+
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
