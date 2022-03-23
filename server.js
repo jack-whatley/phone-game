081538c934ea8ca648c7c2b4a5ec92013294f178
wheatley1239 <wickselm2005@gmail.com>
2022-03-23 09:16:52 +0000

    chat function working

--- server.js
+++ server.js
@@ -15,7 +15,14 @@ app.get("/", function(req, res) {
 
 io.on("connection", (socket) => {
 
-    console.log("a user connected");
+    //console.log("a user connected");
+
+    socket.on("chat message", (msg) => {
+        
+        // console.log("message: " + msg);
+        io.emit("chat message", msg);
+      
+    });
 
     socket.on("disconnect", () => {
 
