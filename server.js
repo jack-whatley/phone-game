1c98772bee34c01050eca716134cd830b48777c5
wheatley1239 <wickselm2005@gmail.com>
2022-03-26 15:59:57 +0000

    room system + username handler

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const io = require("socket.io")(server);
 app.use(express.static(__dirname + "/node_modules"));
 app.use(express.static(__dirname + "/public"));
 
-let playerList = [];
+let roomList = [];
+let users = [];
 
 app.get("/", function(req, res) {
 
@@ -19,7 +20,28 @@ io.on("connection", (socket) => {
 
     console.log(`user id: ${socket.id}`); // this works
 
-    
+    socket.on("host game", (userId) => {
+
+        let roomCode = Math.floor(Math.random() * 1000000) + 100000;
+        console.log(roomCode);
+        socket.join(roomCode);
+        io.sockets.in(roomCode).emit("connectedRoom", roomCode);
+
+    });
+
+    socket.on("join game", (code) => {
+
+        socket.join(code);
+        io.sockets.in(code).emit("connectedRoom", code);
+
+    });
+
+    socket.on("submit name", (id, username, room) => {
+
+        users.push({id: id, name: username, room: room});
+        console.log(users);
+
+    });
 
     socket.on("disconnect", () => {
 
